954fc7db9454647fa64090c40538569998103561
Lucas Trevizan de Freitas <lukas_tf@hotmail.com>
2025-07-22 12:26:43 -0300

    removed lint disablers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable no-console */
 import React from 'react';
 import { UserWarning } from './UserWarning';
 import { USER_ID } from './api/todos';
